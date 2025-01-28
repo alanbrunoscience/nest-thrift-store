e5a577995b29194354a974e9393d439bff8df66b
alanbrunoscience <alanbrunomath@gmail.com>
2025-01-28 09:03:03 -0300

    :sparkles: Insert Product resource with relationship

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categoria/categoria.module';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'db_thrift_store',
-    autoLoadEntities: true,
-    synchronize: true,
-    logging: true
-  }),
-  // Inserir nome da classe module de cada entidade (ex.: PostagemModule)
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_thrift_store',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+    CategoriaModule,
+    ProdutoModule,
   ],
   controllers: [],
   providers: [],
